9e23bf26bd83a8456957a3e7c90003adb5e5e848
cchoi1 <carolinechoi2000@gmail.com>
2022-08-03 11:05:46 -0700

    update scripts

--- scripts/eval-fix/precipitation.sh
+++ scripts/eval-fix/precipitation.sh
@@ -14,13 +14,13 @@ python main.py --dataset=precipitation --method=erm --offline --mini_batch_size=
 python main.py --dataset=precipitation --method=erm --lisa --offline --mix_alpha=2.0 --mini_batch_size=128 --train_update_iter=5000 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
 
 #A-GEM
-python main.py --dataset=precipitation --method=agem --buffer_size=1000 --offline --mini_batch_size=128 --train_update_iter=500 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
+python main.py --dataset=precipitation --method=agem --buffer_size=1000 --offline --mini_batch_size=128 --train_update_iter=5000 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
 
 #EWC
-python main.py --dataset=precipitation --method=ewc --ewc_lambda=0.1 --online --offline --mini_batch_size=128 --train_update_iter=500 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
+python main.py --dataset=precipitation --method=ewc --ewc_lambda=0.1 --online --offline --mini_batch_size=128 --train_update_iter=5000 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
 
 #Fine-tuning
-python main.py --dataset=precipitation --method=ft --offline --mini_batch_size=128 --train_update_iter=500 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
+python main.py --dataset=precipitation --method=ft --offline --mini_batch_size=128 --train_update_iter=5000 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
 
 #SI
-python main.py --dataset=precipitation --method=si --si_c=0.1 --epsilon=0.001 --offline --mini_batch_size=128 --train_update_iter=500 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
+python main.py --dataset=precipitation --method=si --si_c=0.1 --epsilon=0.001 --offline --mini_batch_size=128 --train_update_iter=5000 --lr=0.001 --split_time=7 --random_seed=1 --log_dir=./checkpoints
