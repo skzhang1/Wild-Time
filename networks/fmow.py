e24babfa7ce7da61e760738e9d9bbabee88bd193
cchoi1 <carolinechoi2000@gmail.com>
2022-08-08 22:19:18 -0700

    modify FMoW and Yearbook networks to support SimCLR

--- networks/fmow.py
+++ networks/fmow.py
@@ -4,26 +4,36 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import densenet121
+from lightly.models.modules.heads import SimCLRProjectionHead
 
 IMG_HEIGHT = 224
 NUM_CLASSES = 62
 
 class FMoWNetwork(nn.Module):
-    def __init__(self, weights=None):
+    def __init__(self, args, weights=None, ssl_training=False):
         super(FMoWNetwork, self).__init__()
+        self.args = args
         self.num_classes = NUM_CLASSES
         self.enc = densenet121(pretrained=True).features
         self.classifier = nn.Linear(1024, self.num_classes)
         if weights is not None:
             self.load_state_dict(deepcopy(weights))
+        # Projection head for SimCLR
+        self.projection_head = SimCLRProjectionHead(50176, 50176, 128)
+        self.ssl_training = ssl_training
 
     def reset_weights(self, weights):
         self.load_state_dict(deepcopy(weights))
 
     def forward(self, x):
         features = self.enc(x)
-        out = F.relu(features, inplace=True)
-        out = F.adaptive_avg_pool2d(out, (1, 1))
-        out = torch.flatten(out, 1)
-        out = self.classifier(out)
-        return out
+
+        if self.args.method == 'simclr' and self.ssl_training:
+            h = features.flatten(start_dim=1)
+            return self.projection_head(h)
+        else:
+            out = F.relu(features, inplace=True)
+            out = F.adaptive_avg_pool2d(out, (1, 1))
+            out = torch.flatten(out, 1)
+            out = self.classifier(out)
+            return out
--- networks/yearbook.py
+++ networks/yearbook.py
@@ -1,17 +1,18 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
+from lightly.models.modules.heads import SimCLRProjectionHead
 
 class YearbookNetwork(nn.Module):
-    def __init__(self, args, num_input_channels, num_classes):
+    def __init__(self, args, num_input_channels, num_classes, ssl_training=False):
         super(YearbookNetwork, self).__init__()
         self.args = args
-        self.net = nn.Sequential(self.conv_block(num_input_channels, 32), self.conv_block(32, 32),
+        self.enc = nn.Sequential(self.conv_block(num_input_channels, 32), self.conv_block(32, 32),
                                  self.conv_block(32, 32), self.conv_block(32, 32))
         self.hid_dim = 32
-
-        self.logits = nn.Linear(32, num_classes)
+        self.classifier = nn.Linear(32, num_classes)
+        # Projection head for SimCLR
+        self.projection_head = SimCLRProjectionHead(128, 128, 128)
+        self.ssl_training = ssl_training
 
     def conv_block(self, in_channels, out_channels):
         return nn.Sequential(
@@ -22,8 +23,11 @@ class YearbookNetwork(nn.Module):
         )
 
     def forward(self, x):
-        x = self.net(x)
+        x = self.enc(x)
 
-        x = torch.mean(x, dim=(2, 3))
-
-        return self.logits(x)
+        if self.args.method == 'simclr' and self.ssl_training:
+            h = x.flatten(start_dim=1)
+            return self.projection_head(h)
+        else:
+            h = torch.mean(x, dim=(2, 3))
+            return self.classifier(h)
